09295b660971e291e0836391587eb775fea3f76c
Rohit N B <48172220+rohitnb@users.noreply.github.com>
2021-12-07 08:28:42 +0000

    running ncc build

--- dist/index.js
+++ dist/index.js
@@ -11481,24 +11481,32 @@ const github = __nccwpck_require__(9510);
 const axios = __nccwpck_require__(6284);
 
 const targetRepoName = core.getInput('repo-name');
+const ghToken = core.getInput('org-admin-token');
 var createRepoData = JSON.stringify(
   {
     "name":targetRepoName,
   }
 );
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-console.log(JSON.stringify(github.context.payload.repository.owner.login));
+const targetOrgName = JSON.stringify(github.context.payload.repository.owner.login);
 
-// var config = {
-//   method: 'post',
-//   url: 'https://api.github.com/orgs//repos',
-//   headers: { 
-//     'Accept': 'application/vnd.github.v3+json', 
-//     'Authorization': 'token ', 
-//     'Content-Type': 'application/json'
-//   },
-//   data : data
-// };
+var config = {
+  method: 'post',
+  url: 'https://api.github.com/orgs/'+targetOrgName+'/repos',
+  headers: { 
+    'Accept': 'application/vnd.github.v3+json', 
+    'Authorization': 'token '+ghToken, 
+    'Content-Type': 'application/json'
+  },
+  data : createRepoData
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
 })();
 
 module.exports = __webpack_exports__;
