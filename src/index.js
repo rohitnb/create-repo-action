aec817ab81075c339e1dbb546c7df4a097f9226c
Rohit N B <48172220+rohitnb@users.noreply.github.com>
2021-12-07 08:27:17 +0000

    adding REST API to create repo

--- src/index.js
+++ src/index.js
@@ -3,21 +3,29 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 const targetRepoName = core.getInput('repo-name');
+const ghToken = core.getInput('org-admin-token');
 var createRepoData = JSON.stringify(
   {
     "name":targetRepoName,
   }
 );
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-console.log(JSON.stringify(github.context.payload.repository.owner.login));
+const targetOrgName = JSON.stringify(github.context.payload.repository.owner.login);
 
-// var config = {
-//   method: 'post',
-//   url: 'https://api.github.com/orgs//repos',
-//   headers: { 
-//     'Accept': 'application/vnd.github.v3+json', 
-//     'Authorization': 'token ', 
-//     'Content-Type': 'application/json'
-//   },
-//   data : data
-// };
+var config = {
+  method: 'post',
+  url: 'https://api.github.com/orgs/'+targetOrgName+'/repos',
+  headers: { 
+    'Accept': 'application/vnd.github.v3+json', 
+    'Authorization': 'token '+ghToken, 
+    'Content-Type': 'application/json'
+  },
+  data : createRepoData
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
