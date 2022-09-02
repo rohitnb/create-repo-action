820f13aa48c38cada74a7e54800d27ba2ea0b5c4
Morten Duus <morten@duus.me>
2022-09-02 10:00:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios');
 
 const targetRepoName = core.getInput('repo-name');
 const targetOrgName = core.getInput('org-name');
 const ghToken = core.getInput('org-admin-token');
 
-var createRepoData = JSON.stringify(
-  {
-    "name":targetRepoName,
-    "private":true,
-    "visibility":"private"
-  }
-);
 
+const octokit = new Octokit({
+    auth: ghToken
+  })
 
-var config = {
-  method: 'post',
-  url: 'https://api.github.com/orgs/'+targetOrgName+'/repos',
-  headers: { 
-    'Accept': 'application/vnd.github.v3+json', 
-    'Authorization': 'token '+ghToken, 
-    'Content-Type': 'application/json'
-  },
-  data : createRepoData
-};
+await octokit.request('POST /orgs/{org}/repos', {
+    org: targetOrgName,
+    name: targetRepoName,
+    description: 'This is a demo repo created from an action repository',
+    homepage: 'https://github.com',
+    'private': true,
+    has_issues: true,
+    has_projects: true,
+    has_wiki: true
+  })
 
-axios(config)
-.then(function (response) {
-  console.log("Repo "+targetRepoName+' created successfully!');
-  core.setOutput("repo-url", "https://github.com/"+targetOrgName+"/"+targetRepoName);
-})
-.catch(function (error) {
-  core.setOutput("repo-url", "");
-  core.setFailed(error.message);
-});
